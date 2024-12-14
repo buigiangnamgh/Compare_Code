aafc2ee22d1ee0564f78cbc7a0ccadac89d1489c
buigiangnamgh <giangnamdhv@gmail.com>
2024-12-14 11:26:48 +0700

    Nam code

--- HIS.UC.ExamTreatmentFinish/Run/RunBehavior.cs
+++ HIS.UC.ExamTreatmentFinish/Run/RunBehavior.cs
@@ -1,21 +1,4 @@
-/* IVT
- * @Project : hisnguonmo
- * Copyright (C) 2017 INVENTEC
- *  
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *  
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
- * GNU General Public License for more details.
- *  
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
- */
-
+﻿
 using HIS.UC.ExamTreatmentFinish.ADO;
 using Inventec.Core;
 using System;
@@ -29,24 +12,22 @@ namespace HIS.UC.ExamTreatmentFinish.Run
     public sealed class RunBehavior : IRun
     {
         TreatmentFinishInitADO entity;
-        MOS.EFMODEL.DataModels.HIS_TREATMENT_EXT _treatmentExt;
         public RunBehavior()
             : base()
         {
         }
 
-        public RunBehavior(CommonParam param, TreatmentFinishInitADO data,MOS.EFMODEL.DataModels.HIS_TREATMENT_EXT treatmentExt)
+        public RunBehavior(CommonParam param, TreatmentFinishInitADO data)
             : base()
         {
             this.entity = data;
-            this._treatmentExt = treatmentExt;
         }
 
         object IRun.Run()
         {
             try
             {
-                return new UCExamTreatmentFinish(entity,_treatmentExt);
+                return new UCExamTreatmentFinish(entity);
             }
             catch (Exception ex)
             {
--- HIS.UC.ExamTreatmentFinish/Run/UCTreatmentFinish___Validate.cs
+++ HIS.UC.ExamTreatmentFinish/Run/UCTreatmentFinish___Validate.cs
@@ -1,21 +1,4 @@
-/* IVT
- * @Project : hisnguonmo
- * Copyright (C) 2017 INVENTEC
- *  
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *  
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
- * GNU General Public License for more details.
- *  
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
- */
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Drawing;
@@ -44,9 +27,9 @@ namespace HIS.UC.ExamTreatmentFinish.Run
             ValidateCombo(cboTreatmentEndType);
             ValidateCombo(cboTreatmentResult);
             ValidationICD(10, 500, !this.isAllowNoIcd);
+            ValidationICDYhct(10, 500, false);
             ValidateComboCareer();
             ValidationControlMaxLength(memNote, 2000);
-            
         }
 
         private void ValidationControlMaxLength(BaseEdit control, int? maxLength)
@@ -59,22 +42,6 @@ namespace HIS.UC.ExamTreatmentFinish.Run
             this.dxValidationProvider1.SetValidationRule(control, validate);
         }
 
-        private void ValidateComboCareer()
-        {
-            try
-            {
-                GridLookupEditValidationRule validationRule = new GridLookupEditValidationRule();
-                validationRule.cbo = cboCareer;
-                //validationRule.ErrorText = Inventec.Desktop.Common.LibraryMessage.MessageUtil.GetMessage(Inventec.Desktop.Common.LibraryMessage.Message.Enum.TruongDuLieuBatBuoc);
-                validationRule.ErrorText = "Bắt buộc nhập thông tin nghề nghiệp";
-                validationRule.ErrorType = ErrorType.Warning;
-                dxValidationProvider1.SetValidationRule(cboCareer, validationRule);
-            }
-            catch (Exception ex)
-            {
-                Inventec.Common.Logging.LogSystem.Warn(ex);
-            }
-        }
         private void ValidateSignHead()
         {
 
@@ -110,6 +77,24 @@ namespace HIS.UC.ExamTreatmentFinish.Run
                 Inventec.Common.Logging.LogSystem.Warn(ex);
             }
         }
+
+        private void ValidateComboCareer()
+        {
+            try
+            {
+                GridLookupEditValidationRule validationRule = new GridLookupEditValidationRule();
+                validationRule.cbo = cboCareer;
+                //validationRule.ErrorText = Inventec.Desktop.Common.LibraryMessage.MessageUtil.GetMessage(Inventec.Desktop.Common.LibraryMessage.Message.Enum.TruongDuLieuBatBuoc);
+                validationRule.ErrorText = "Bắt buộc nhập thông tin nghề nghiệp";
+                validationRule.ErrorType = ErrorType.Warning;
+                dxValidationProvider1.SetValidationRule(cboCareer, validationRule);
+            }
+            catch (Exception ex)
+            {
+                Inventec.Common.Logging.LogSystem.Warn(ex);
+            }
+        }
+
         private void ValidationTimeFinish()
         {
             try
