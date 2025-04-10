a441eb3d14d258e67b913bdfc1612976f3bab48a
buigiangnamgh <giangnamdhv@gmail.com>
2025-04-10 09:40:48 +0700

    VSS code

--- HIS.Desktop.Plugins.AssignService/AssignService/frmAssignService__UcDate.cs
+++ HIS.Desktop.Plugins.AssignService/AssignService/frmAssignService__UcDate.cs
@@ -1,6 +1,6 @@
 /* IVT
  * @Project : hisnguonmo
- * Copyright (C) 2017 INVENTEC
+ * Copyright (C) 2017 INVENTEC 1
  *  
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
