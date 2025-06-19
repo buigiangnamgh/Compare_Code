818d3bd95c5a355eaa70ef70df98ae5256f87a8e
buigiangnamgh <giangnamdhv@gmail.com>
2025-06-19 07:44:49 +0700

    code cũ

---
+++ Inventec.Common.SignLibrary/frmConfirmSign.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace Inventec.Common.SignLibrary
+{
+    partial class frmConfirmSign
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.btnYes = new DevExpress.XtraEditors.SimpleButton();
+            this.btnNo = new DevExpress.XtraEditors.SimpleButton();
+            this.chkState = new DevExpress.XtraEditors.CheckEdit();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chkState.Properties)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(25, 21);
+            this.lblTitle.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(679, 66);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Tài khoản thiếu thông tin ảnh, bạn có muốn bỏ hiển thị ảnh chữ ký không?";
+            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnYes
+            // 
+            this.btnYes.Location = new System.Drawing.Point(244, 156);
+            this.btnYes.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnYes.Name = "btnYes";
+            this.btnYes.Size = new System.Drawing.Size(100, 28);
+            this.btnYes.TabIndex = 1;
+            this.btnYes.Text = "Có";
+            this.btnYes.Click += new System.EventHandler(this.btnYes_Click);
+            // 
+            // btnNo
+            // 
+            this.btnNo.Location = new System.Drawing.Point(352, 156);
+            this.btnNo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnNo.Name = "btnNo";
+            this.btnNo.Size = new System.Drawing.Size(100, 28);
+            this.btnNo.TabIndex = 1;
+            this.btnNo.Text = "Không";
+            this.btnNo.Click += new System.EventHandler(this.btnNo_Click);
+            // 
+            // chkState
+            // 
+            this.chkState.Location = new System.Drawing.Point(68, 91);
+            this.chkState.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.chkState.Name = "chkState";
+            this.chkState.Properties.Caption = "Lưu lại lựa chọn cho các lần ký sau";
+            this.chkState.Size = new System.Drawing.Size(611, 21);
+            this.chkState.TabIndex = 2;
+            this.chkState.CheckedChanged += new System.EventHandler(this.chkState_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(84, 118);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(368, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = " (Lưu ý: nhấn Ctrl + Shift + R để xóa trạng thái đã lưu)";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // frmConfirmSign
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(728, 197);
+            this.Controls.Add(this.chkState);
+            this.Controls.Add(this.btnNo);
+            this.Controls.Add(this.btnYes);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblTitle);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmConfirmSign";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Hỏi đáp";
+            this.Load += new System.EventHandler(this.frmConfirmSign_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chkState.Properties)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private DevExpress.XtraEditors.SimpleButton btnYes;
+        private DevExpress.XtraEditors.SimpleButton btnNo;
+        private DevExpress.XtraEditors.CheckEdit chkState;
+        private System.Windows.Forms.Label label1;
+    }
+}
